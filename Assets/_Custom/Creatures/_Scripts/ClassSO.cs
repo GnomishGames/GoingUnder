308041b9d67d895ade053fcc9bda0f893ee8ef84
Clay Ashby <kingpoiuy@gmail.com>
2026-02-27 07:32:33 -0500

    feat: Enhance Class and Race scriptable objects with detailed tooltips and headers

--- Assets/_Custom/Creatures/_Scripts/ClassSO.cs
+++ Assets/_Custom/Creatures/_Scripts/ClassSO.cs
@@ -5,14 +5,21 @@ public class ClassSO : ScriptableObject
 {
     public string className;
 
+    [Header("Hit and Mana die affect how much health and mana the creature has.")]
     public int hitDie;
     public int manaDie;
 
-    //stats
+    [Header("Stat bonuses are special skills developed by the class. 1 is normal.")]
+    [Tooltip("A warrior might have more strength than they would appear to have at first glance.")]
     public float strengthBonus;
+    [Tooltip("A rogue might have more dexterity than they would appear to have at first glance.")]
     public float dexterityBonus;
+    [Tooltip("A paladin might have more constitution than they would appear to have at first glance.")]
     public float constitutionBonus;
+    [Tooltip("A wizard might have more intelligence than they would appear to have at first glance.")]
     public float intelligenceBonus;
+    [Tooltip("A cleric might have more wisdom than they would appear to have at first glance.")]
     public float wisdomBonus;
+    [Tooltip("A bard might have more charisma than they would appear to have at first glance.")]
     public float charismaBonus;
 }
--- Assets/_Custom/Creatures/_Scripts/RaceSO.cs
+++ Assets/_Custom/Creatures/_Scripts/RaceSO.cs
@@ -1,39 +1,33 @@
 using UnityEngine;
 
-public enum Size
-{
-    Colossal,   // -8
-    Gargantuan, // -4
-    Huge,       // -2
-    Large,      // -1
-    Medium,     // +0
-    Small,      // +1
-    Tiny,       // +2
-    Diminutive, // +4
-    Fine        // +8
-}
-
 [CreateAssetMenu(fileName = "New Race", menuName = "Scriptable Object/Character/Race")]
 public class RaceSO : ScriptableObject
 {
     public string raceName;
 
-    //racial bonuses
-    public int strengthBonus;
-    public int constitutionBonus;
-    public int dexterityBonus;
-    public int intelligenceBonus;
-    public int wisdomBonus;
-    public int charismaBonus;
+    [Header("Racial bonuses should be 1 unless the race has special properties.")]
+    [Tooltip("Creatures like a zombie might have more strength than they would appear to have at first glance.")]
+    public int strengthBonus = 1;
+    [Tooltip("Creatures like a dwarf might have more constitution than they would appear to have at first glance.")]
+    public int constitutionBonus = 1;
+    [Tooltip("Creatures like an elf might have more dexterity than they would appear to have at first glance.")]
+    public int dexterityBonus = 1;
+    [Tooltip("Creatures like a gnome might have more intelligence than they would appear to have at first glance.")]
+    public int intelligenceBonus = 1;
+    [Tooltip("Creatures like a halfling might have more wisdom than they would appear to have at first glance.")]
+    public int wisdomBonus = 1;
+    [Tooltip("Creatures like a vampire might have more charisma than they would appear to have at first glance.")]
+    public int charismaBonus = 1;
 
-    //attack vars
-    public float attackSpeed;
-
-    //defense vars
+    [Header("The size of the creature affects its AC.")] 
+    [Tooltip("Larger creatures are easier to hit, while smaller creatures are harder to hit.")]
     public Size size = Size.Medium;
+    
+    [Header("Creatures that have some kind of natural armor (like a turtle shell) can have a natural AC bonus.")]
+    [Tooltip("This is added to the size AC bonus and any armor the creature is wearing to determine total AC.")]
     public int naturalAcBonus;
 
-    //view vars
+    [Header("The creature's vision range and angle. Unlikely to ever be used in this game.")]
     public float viewRadius = 100f;
     public float viewAngle = 180f;
 
@@ -65,5 +59,18 @@ public class RaceSO : ScriptableObject
                     return 0;
             }
         }
+    }
+
+    public enum Size
+    {
+        Colossal,   // -8
+        Gargantuan, // -4
+        Huge,       // -2
+        Large,      // -1
+        Medium,     // +0
+        Small,      // +1
+        Tiny,       // +2
+        Diminutive, // +4
+        Fine        // +8
     }
 }
