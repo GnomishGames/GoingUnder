1ae2d0d1b5529587f8ae81021f9552c890c99255
Clay Ashby <kingpoiuy@gmail.com>
2026-03-01 08:08:19 -0500

    Hover tooltip for inventory refined.

--- Assets/_Custom/GUI/_Scripts/ItemValueUpdate.cs
+++ Assets/_Custom/GUI/_Scripts/ItemValueUpdate.cs
@@ -69,7 +69,7 @@ public class ItemValueUpdate : MonoBehaviour
     {
         if (inventory == null || parentSlot == null)
         {
-            itemValueText.text = "";
+            itemValueText.text = "0";
             return;
         }
 
@@ -103,13 +103,123 @@ public class ItemValueUpdate : MonoBehaviour
                     itemValueText.text = item.slotType.ToString();
                     break;
                 case "ArmorBonus":
-                    itemValueText.text = item is EquipmentSO equipment ? equipment.ArmorBonus.ToString() : "";
+                    if (item is EquipmentSO equipment && equipment.ArmorBonus != 0)
+                    {
+                        itemValueText.text = "ac: " + equipment.ArmorBonus.ToString();
+                    }
+                    else
+                    {
+                        this.gameObject.transform.parent.gameObject.SetActive(false);
+                    }
                     break;
                 case "StaminaBonus":
-                    itemValueText.text = item is EquipmentSO equipment2 ? equipment2.StaminaBonus.ToString() : "";
+                    if (item is EquipmentSO equipment2 && equipment2.StaminaBonus != 0)
+                    {
+                        itemValueText.text = "sta: " + equipment2.StaminaBonus.ToString();
+                    }
+                    else
+                    {
+                        this.gameObject.transform.parent.gameObject.SetActive(false);
+                    }
                     break;
                 case "ManaBonus":
-                    itemValueText.text = item is EquipmentSO equipment3 ? equipment3.ManaBonus.ToString() : "";
+                    if (item is EquipmentSO equipment3 && equipment3.ManaBonus != 0)
+                    {
+                        itemValueText.text = "mana: " + equipment3.ManaBonus.ToString();
+                    }
+                    else
+                    {
+                        this.gameObject.transform.parent.gameObject.SetActive(false);
+                    }
+                    break;
+                case "StrengthBonus":
+                    if (item is EquipmentSO equipment4 && equipment4.StrengthBonus != 0)
+                    {
+                        itemValueText.text = "str: " + equipment4.StrengthBonus.ToString();
+                    }
+                    else
+                    {
+                        //disable the parent GO if the item doesn't have this stat
+                        this.gameObject.transform.parent.gameObject.SetActive(false);
+                    }
+                    break;
+                case "ConstitutionBonus":
+                    if (item is EquipmentSO equipment5 && equipment5.ConstitutionBonus != 0)
+                    {
+                        itemValueText.text = "con: " + equipment5.ConstitutionBonus.ToString();
+                    }
+                    else
+                    {
+                        //disable the parent GO if the item doesn't have this stat
+                        this.gameObject.transform.parent.gameObject.SetActive(false);
+                    }
+                    break;
+                case "DexterityBonus":
+                    if (item is EquipmentSO equipment6 && equipment6.DexterityBonus != 0)
+                    {
+                        itemValueText.text = "dex: " + equipment6.DexterityBonus.ToString();
+                    }
+                    else
+                    {
+                        //disable the parent GO if the item doesn't have this stat
+                        this.gameObject.transform.parent.gameObject.SetActive(false);
+                    }
+                    break;
+                case "IntelligenceBonus":
+                    if (item is EquipmentSO equipment7 && equipment7.IntelligenceBonus != 0)
+                    {
+                        itemValueText.text = "int: " + equipment7.IntelligenceBonus.ToString();
+                    }
+                    else
+                    {
+                        //disable the parent GO if the item doesn't have this stat
+                        this.gameObject.transform.parent.gameObject.SetActive(false);
+                    }
+                    break;
+                case "WisdomBonus":
+                    if (item is EquipmentSO equipment8 && equipment8.WisdomBonus != 0)
+                    {
+                        itemValueText.text = "wis: " + equipment8.WisdomBonus.ToString();
+                    }
+                    else
+                    {
+                        //disable the parent GO if the item doesn't have this stat
+                        this.gameObject.transform.parent.gameObject.SetActive(false);
+                    }
+                    break;
+                case "CharismaBonus":
+                    if (item is EquipmentSO equipment9 && equipment9.CharismaBonus != 0)
+                    {
+                        itemValueText.text = "cha: " + equipment9.CharismaBonus.ToString();
+                    }
+                    else
+                    {
+                        //disable the parent GO if the item doesn't have this stat
+                        this.gameObject.transform.parent.gameObject.SetActive(false);
+                    }
+                    break;
+                case "Critical":
+                    if (item is EquipmentSO equipment12 && (equipment12.CriticalChanceBonus != 0 || equipment12.CriticalDamageBonus != 0))
+                    {
+                        itemValueText.text = "crit: " + equipment12.CriticalChanceBonus.ToString() + "% @ " + equipment12.CriticalDamageBonus.ToString() + "x";
+                    }
+                    else
+                    {
+                        this.gameObject.transform.parent.gameObject.SetActive(false);
+                    }
+                    break;
+                case "DamageType":
+                    itemValueText.text = item is WeaponSO weapon ? weapon.damageType.ToString() : "";
+                    break;
+                case "Die":
+                    if (item is WeaponSO weapon3 && (weapon3.DieMultiplier != 0 || weapon3.DieBonus != 0))
+                    {
+                        itemValueText.text = weapon3.DieMultiplier.ToString() + "d" + weapon3.DieBonus.ToString();
+                    }
+                    else
+                    {
+                        this.gameObject.transform.parent.gameObject.SetActive(false);
+                    }
                     break;
                 default:
                     itemValueText.text = "";
